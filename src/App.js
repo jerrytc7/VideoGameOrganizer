98e44e6789bbe2df0f1fbfc70d1711247e3bdca1
jerrytc7 <jerrykondner@gmail.com>
2021-06-16 19:52:32 -0400

    adding react router  and test route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Component } from 'react';
 import './App.css';
+import Home from "./compenents/Home"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+        <Router>
+            <Switch>
+              <Route exact path= "/" compenent={Home}/>
+            </Switch>
+        </Router>      
     </div>
   );
 }
