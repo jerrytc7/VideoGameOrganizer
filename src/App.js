8a793475043cae35e125484cbde91e5fb017220d
jerrytc7 <jerrykondner@gmail.com>
2021-07-19 18:37:41 -0400

    3 routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Component } from 'react';
 import './App.css';
-import Home from "./compenents/Home"
+import Home from "./components/Home"
+import About from "./components/About"
+import Login from "./components/Login"
+import Navbar from "./components/Navbar"
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -8,7 +10,10 @@ function App() {
     <div className="App">
         <Router>
             <Switch>
-              <Route exact path= "/" compenent={Home}/>
+              <Route exact path= "/home" component={Home}/>
+              <Route exact path= "/about" component={About}/>
+              <Route exact path= "/login" component={Login}/>
+              <Route exact path= "/" component={Navbar}/>
             </Switch>
         </Router>      
     </div>
